cd80cc1c51dba366a4d067045b1cf681c99cc885
vitorGabr <macstudios.info@gmail.com>
2025-10-26 13:01:50 -0300

    refactor: remover o ClerkProvider do layout e simplificar a estrutura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import "./global.css";
-import { ptBR } from "@clerk/localizations";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
@@ -27,34 +25,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
 	return (
-		<ClerkProvider
-			localization={ptBR}
-			appearance={{
-				variables: {
-					colorPrimary: "var(--primary)",
-					colorForeground: "var(--foreground)",
-					colorBackground: "var(--background)",
-				},
-				elements: {
-					formButtonPrimary: "bg-slate-500 hover:bg-slate-400 text-sm",
-				},
-			}}
-		>
-			<html lang="pt-BR">
-				<body className={`${spaceGrotesk.variable} font-sans antialiased`}>
-					<Suspense>
-						<Providers>
-							<div className="relative flex size-full min-h-screen flex-col bg-[#f8fbfa] group/design-root">
-								{children}
-								<Footer />
-								<Toaster />
-								<SpeedInsights />
-								<Analytics />
-							</div>
-						</Providers>
-					</Suspense>
-				</body>
-			</html>
-		</ClerkProvider>
+		<html lang="pt-BR">
+			<body className={`${spaceGrotesk.variable} font-sans antialiased`}>
+				<Suspense>
+					<Providers>
+						<div className="relative flex size-full min-h-screen flex-col bg-[#f8fbfa] group/design-root">
+							{children}
+							<Footer />
+							<Toaster />
+							<SpeedInsights />
+							<Analytics />
+						</div>
+					</Providers>
+				</Suspense>
+			</body>
+		</html>
 	);
 }
