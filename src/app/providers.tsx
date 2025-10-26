e04c24d385e19e5495f090c97114d1c288499e5a
vitorGabr <macstudios.info@gmail.com>
2025-10-26 20:55:38 -0300

    refactor: remover integração do Clerk e simplificar a estrutura do AuthSection

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { ptBR } from "@clerk/localizations";
-import { ClerkProvider } from "@clerk/nextjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NuqsAdapter } from "nuqs/adapters/next";
 import { useState } from "react";
@@ -20,24 +18,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
 	);
 
 	return (
-		<ClerkProvider
-			localization={ptBR}
-			appearance={{
-				variables: {
-					colorPrimary: "var(--primary)",
-					colorForeground: "var(--foreground)",
-					colorBackground: "var(--background)",
-				},
-				elements: {
-					formButtonPrimary: "bg-slate-500 hover:bg-slate-400 text-sm",
-				},
-			}}
-		>
-			<NuqsAdapter>
-				<QueryClientProvider client={queryClient}>
-					{children}
-				</QueryClientProvider>
-			</NuqsAdapter>
-		</ClerkProvider>
+		<NuqsAdapter>
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		</NuqsAdapter>
 	);
 }
--- src/components/auth-section.tsx
+++ src/components/auth-section.tsx
@@ -1,57 +1,9 @@
 "use client";
 
-import {
-	SignedIn,
-	SignedOut,
-	SignInButton,
-	SignUpButton,
-	UserButton,
-} from "@clerk/nextjs";
-import { Calendar1Icon, PawPrintIcon, User as UserIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { NotificationMenu } from "./notification-menu";
-
 export const AuthSection = () => {
 	return (
 		<>
-			<SignedIn>
-				<div className="flex items-center gap-4">
-					<NotificationMenu />
-					<UserButton>
-						<UserButton.MenuItems>
-							<UserButton.Link
-								label="Agendamentos"
-								labelIcon={<Calendar1Icon className="size-4" />}
-								href="/appointments"
-							/>
-							<UserButton.Link
-								label="Meus Pets"
-								labelIcon={<PawPrintIcon className="size-4" />}
-								href="/pets"
-							/>
-						</UserButton.MenuItems>
-					</UserButton>
-				</div>
-			</SignedIn>
-			<SignedOut>
-				<div className="flex items-center gap-4">
-					<SignInButton mode="modal">
-						<Button
-							variant="outline"
-							className="flex items-center gap-2 px-4 py-2 rounded-full"
-						>
-							<UserIcon className="h-4 w-4" />
-							Entrar
-						</Button>
-					</SignInButton>
-					<SignUpButton mode="modal">
-						<Button className="flex items-center gap-2 px-4 py-2 rounded-full bg-primary text-primary-foreground hover:bg-primary/90">
-							<UserIcon className="h-4 w-4" />
-							Criar conta
-						</Button>
-					</SignUpButton>
-				</div>
-			</SignedOut>
+			
 		</>
 	);
 };
