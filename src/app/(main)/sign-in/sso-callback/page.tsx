f6792213358fe14bd1e23b0014265fab5809dda6
vitorGabr <macstudios.info@gmail.com>
2025-10-27 13:20:09 -0300

    fix: adicionar mensagem obrigatória para o campo de arquivo no schema de criação de pet

--- src/app/(main)/sign-in/sso-callback/page.tsx
+++ src/app/(main)/sign-in/sso-callback/page.tsx
@@ -2,9 +2,9 @@ import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 
 export default function Page() {
 	return (
-		<>
+		<div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
 			<AuthenticateWithRedirectCallback />
 			<div id="clerk-captcha" />
-		</>
+		</div>
 	);
 }
--- src/schemas/create-pet.ts
+++ src/schemas/create-pet.ts
@@ -5,7 +5,7 @@ export const createPetSchema = z.object({
 	age: z.number().min(0, "A idade deve ser um número não negativo"),
 	breedId: z.string().min(1, "A raça é obrigatória"),
 	weight: z.number().min(0, "O peso deve ser um número não negativo"),
-	file: z.file().max(5 * 1024 * 1024, "A imagem tem que ser menor que 5MB"),
+	file: z.file('A imagem é obrigatória').max(5 * 1024 * 1024, "A imagem tem que ser menor que 5MB"),
 });
 
 export type CreatePetSchema = z.infer<typeof createPetSchema>;
